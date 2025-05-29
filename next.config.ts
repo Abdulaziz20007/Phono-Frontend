4c97a54aec0291c8e1e1052ab4daccc68ded0373
Abdulaziz20007 <lil.gangsser@gmail.com>
2025-05-29 11:24:03 +0500

    done

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    // allowedDevOrigins: [
-    //   "http://142.93.135.99:3000", // Your network IP
-    //   // Add any other origins you use for development
-    // ],
+    allowedDevOrigins: [
+      "http://142.93.135.99:3000", // Your network IP
+      // Add any other origins you use for development
+    ],
   },
 };
 
